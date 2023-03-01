e80d6b56677cab391433a6f70d851892c30b75f1
mnop492 <126656510+mnop492@users.noreply.github.com>
2023-03-01 18:37:04 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,78 +8,66 @@ class MultiOrderedDict(OrderedDict):
         else:
             super(MultiOrderedDict, self).__setitem__(key, value)
             
+class Config():
+    appKey=None
+    appName=None
+    appVersion=None
+    createTokenPwd=None
+    deviceId=None
+    deviceName=None
+    encrypy=None
+    osVersion=None
+    passwordType=None
+    platform=None        
+    account=None
+    enableProxy=None
+    server=None
+    port=None
 
-appKey=None
-appName=None
-appVersion=None
-createTokenPwd=None
-deviceId=None
-deviceName=None
-encrypy=None
-osVersion=None
-passwordType=None
-platform=None        
-account=None
-enableProxy=None
-server=None
-port=None
+    enableProxy=None
+    server=None
+    port=None
 
-enableProxy=None
-server=None
-port=None
+    size=None
+    startDate=None
+    endDate=None
+    login_info_list=[]
 
-size=None
-startDate=None
-endDate=None
-login_info_list=[]
+    def __init__(self, path):
+        config = configparser.RawConfigParser(dict_type=MultiOrderedDict, strict=False)
+        config.read([path])
 
-def loadConfig():
-    global appKey, appName, appVersion,createTokenPwd, deviceId, deviceName,encrypy
-    global osVersion, passwordType, password, platform
-    # global account, password, sign 
-    global enableProxy, server, port
-    global size, startDate, endDate
+        #load INFO section
+        self.appKey=config.get('INFO', 'appKey')
+        self.appName=config.get('INFO', 'appName')
+        self.appVersion=config.get('INFO', 'appVersion')
+        self.createTokenPwd=config.get('INFO', 'createTokenPwd')
+        self.deviceId=config.get('INFO', 'deviceId')
+        self.deviceName=config.get('INFO', 'deviceName')
+        self.encrypy=config.get('INFO', 'encrypy')
+        self.osVersion=config.get('INFO', 'osVersion')
+        self.passwordType=config.get('INFO', 'passwordType')
+        self.platform=config.get('INFO', 'platform')        
 
-    config = configparser.RawConfigParser(dict_type=MultiOrderedDict, strict=False)
-    config.read(['config.ini'])
+        #load PROXY section
+        self.enableProxy=config.get('PROXY', 'enableProxy')
+        self.server=config.get('PROXY', 'server')
+        self.port=config.get('PROXY', 'port')
 
-    #load INFO section
-    appKey=config.get('INFO', 'appKey')
-    appName=config.get('INFO', 'appName')
-    appVersion=config.get('INFO', 'appVersion')
-    createTokenPwd=config.get('INFO', 'createTokenPwd')
-    deviceId=config.get('INFO', 'deviceId')
-    deviceName=config.get('INFO', 'deviceName')
-    encrypy=config.get('INFO', 'encrypy')
-    osVersion=config.get('INFO', 'osVersion')
-    passwordType=config.get('INFO', 'passwordType')
-    platform=config.get('INFO', 'platform')        
+        #load SALEREPORT section
+        self.size=config.get('SALEREPORT', 'size')
+        self.startDate=config.get('SALEREPORT', 'startDate')
+        self.endDate=config.get('SALEREPORT', 'endDate')
 
-    #load PROXY section
-    enableProxy=config.get('PROXY', 'enableProxy')
-    server=config.get('PROXY', 'server')
-    port=config.get('PROXY', 'port')
-
-    #load SALEREPORT section
-    size=config.get('SALEREPORT', 'size')
-    startDate=config.get('SALEREPORT', 'startDate')
-    endDate=config.get('SALEREPORT', 'endDate')
-
-    processLoginInfo(config.get('LOGIN', 'loginInfo'))
-    
-def processLoginInfo(loginInfo):
-    login_list = loginInfo.split('\n')
-    print(login_list)
-    global login_info_list
-    for userInfo in login_list:
-        login = {}
-        userInfo_list = userInfo.split(',')
-        login.update({'account':userInfo_list[0]})
-        login.update({'password':userInfo_list[1]})
-        login.update({'sign':userInfo_list[2]})
-        print(login['account'], login['password'], login['sign'])      
-        login_info_list.append(login)
-    return None
-
-loadConfig()
-print(login_info_list)
+        login_list = config.get('LOGIN', 'loginInfo').split('\n')
+        print(login_list)
+                
+        for userInfo in login_list:
+            login = {}
+            userInfo_list = userInfo.split(',')
+            login.update({'account':userInfo_list[0]})
+            login.update({'password':userInfo_list[1]})
+            login.update({'sign':userInfo_list[2]})
+            # print(login['account'], login['password'], login['sign'])      
+            self.login_info_list.append(login)
+        return None
