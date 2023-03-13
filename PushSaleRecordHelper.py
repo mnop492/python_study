8ef37028a5a125f9c1f659619e37dbad23c53707
mnop492 <126656510+mnop492@users.noreply.github.com>
2023-03-13 16:12:15 +0800

    Update PushSaleRecordHelper.py

--- PushSaleRecordHelper.py
+++ PushSaleRecordHelper.py
@@ -3,6 +3,8 @@ from SaleRecord import SaleRecord
 
 class PushSaleRecordHelper():
     push_saleRecord_account_dict = {}
+    cancel_saleRecord_account_dict = {}
+    insert_saleRecord_account_dict = {}
 
     def __init__(self, pushExcelFile):
         self.pushSaleRecord_df = pd.read_excel(pushExcelFile)
@@ -10,14 +12,73 @@ class PushSaleRecordHelper():
         self.processSaleRecordData(self.pushSaleRecord_df)
 
     def processSaleRecordData(self, pushSaleRecord_df):
-
         for col, row in pushSaleRecord_df.iterrows():
             saleRecord = SaleRecord(row)
-            saleRecordList = None
+            pushSaleRecordList = None
+            cancelSaleRecordList = None
+            insertSaleRecordList = None
             if saleRecord.account in self.push_saleRecord_account_dict:
-                saleRecordList = self.push_saleRecord_account_dict[saleRecord.account]                
+                pushSaleRecordList = self.push_saleRecord_account_dict[saleRecord.account]                
             else:
-                saleRecordList = []   
+                pushSaleRecordList = []   
+            
+            if row['approveStatus'] == 'Cancelled' and row['headerID']:
+                if saleRecord.account in self.cancel_saleRecord_account_dict:
+                    cancelSaleRecordList = self.cancel_saleRecord_account_dict[saleRecord.account]                
+                else:
+                    cancelSaleRecordList = []
+
+            if not row['headerID']:
+                if saleRecord.account in self.insert_saleRecord_account_dict:
+                    insertSaleRecordList = self.insert_saleRecord_account_dict[saleRecord.account]                
+                else:
+                    insertSaleRecordList = []
                 
-            saleRecordList.append(saleRecord)
-            self.push_saleRecord_account_dict.update({saleRecord.account:saleRecordList})
+            pushSaleRecordList.append(saleRecord)
+            self.push_saleRecord_account_dict.update({saleRecord.account:pushSaleRecordList})
+
+            cancelSaleRecordList.append(saleRecord)
+            self.cancel_saleRecord_account_dict.update({saleRecord.account:cancelSaleRecordList})
+
+            insertSaleRecordList.append(saleRecord)
+            self.insert_saleRecord_account_dict.update({saleRecord.account:insertSaleRecordList})
+
+    def diffSaleRecordByAccount(self, account, querySaleRecordList):
+        diff = False
+        pushSaleRecordList = self.push_saleRecord_account_dict[account]
+        modified_headerID_list = []
+
+        for pushSaleRecord in pushSaleRecordList:
+            for querySaleRecord in querySaleRecordList:
+                if (pushSaleRecord.lineID == querySaleRecord.lineID and querySaleRecord.headerID not in modified_headerID_list):
+                    if pushSaleRecord.hash() !=  querySaleRecord.hash(): #the record is modified
+                        #add headerID to modified_headerID_list
+                        modified_headerID = querySaleRecord.headerID
+                        modified_headerID_list.append()
+                        #cancel all line under same HeaderID
+                        cancelSaleRecordList = self.cancel_saleRecord_account_dict[account]
+                        insertSaleRecordList = self.insert_saleRecord_account_dict[account]
+
+                        if account in self.cancel_saleRecord_account_dict:
+                            cancelSaleRecordList = self.cancel_saleRecord_account_dict[account]                
+                        else:
+                            cancelSaleRecordList = []
+                        #insert all line under same HeaderID
+                        if account in self.insert_saleRecord_account_dict:
+                            insertSaleRecordList = self.insert_saleRecord_account_dict[account]                
+                        else:
+                            insertSaleRecordList = []
+
+                        for modifiedSaleRecord in querySaleRecord :
+                            if modifiedSaleRecord.headerID == modified_headerID:
+                                cancelSaleRecordList.append(modifiedSaleRecord)
+                                self.cancel_saleRecord_account_dict.update({modifiedSaleRecord.account:cancelSaleRecordList})
+
+                                insertSaleRecordList.append(modifiedSaleRecord)
+                                self.insert_saleRecord_account_dict.update({modifiedSaleRecord.account:insertSaleRecordList})
+
+        return diff
+
+
+
+
